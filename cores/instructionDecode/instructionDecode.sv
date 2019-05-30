7e000a045ede093fca4783444a04cb0b91a4acf3
mateusps10 <mateus.p.silva@ufv.br>
2019-05-30 08:11:38 -0300

    movido mux

--- cores/instructionDecode/instructionDecode.sv
+++ cores/instructionDecode/instructionDecode.sv
@@ -2,24 +2,22 @@ module instructionDecode(
     input logic clk,
     input logic reset,
     input logic regDst,
-    input logic aluSrc,
     input logic regWrite,
     input logic [25:0] instruction,
     input logic [31:0] writeData,
-    output logic [31:0] aluInput0,
-    output logic [31:0] aluInput1,
-    output logic [31:0] memoryWriteData,
-    output logic [31:0] branch,
+    output logic [31:0] readRegister0,
+    output logic [31:0] readRegister1,
+    output logic [31:0] immediateExtended,
     output logic [5:0] func,
     output wire [31:0] ioRegisters [31:0]
 );
 
-logic [4:0] muxOutputWriteRegister;
+wire [4:0] muxOutputWriteRegister;
 
-registerDatabase registerDatabase0 (clk, reset, regWrite, instruction[25:21], instruction[20:16], muxOutputWriteRegister, writeData, aluInput0, memoryWriteData, ioRegisters);
-signalExtender16_32bits signalExtender16_32bitsBranch (instruction[15:0], branch);
-mux2_1_5bits muxWriteRegister (regDst, instruction[20:16], instruction[15:11], muxOutputWriteRegister);
-mux2_1_32bits muxData2 (aluSrc, memoryWriteData, branch, aluInput1);
+registerDatabase registerDatabase0 (clk, reset, regWrite, instruction[25:21], instruction[20:16], muxOutputWriteRegister, writeData, readRegister0, readRegister1, ioRegisters);
+
+signalExtender16_32bits signalExtender16_32bitsBranch (instruction[15:0], immediateExtended); // Extend immediate from 16 to 32 bits
+mux2_1_5bits muxWriteRegister (regDst, instruction[20:16], instruction[15:11], muxOutputWriteRegister); // Select RD
 
 assign func = instruction[5:0];
 
